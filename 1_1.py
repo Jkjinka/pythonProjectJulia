8e8e41e2c9307b2ec081ba9f2411e590e4ad25a0
Julia <jkjinka@gmail.com>
2021-10-19 21:24:46 +0300

    1_1

--- 1_1.py
+++ 1_1.py
@@ -1,3 +1,9 @@
 n1 = 1
 n2 = 2
-print(n1, n2)
+print("Решите пример:")
+print(n1, "+", n2)
+n3 = input("Введите ответ цифрой:")
+n3str = input("Введите ответ буквами:")
+print("Ваши ответы:")
+print(n3)
+print(n3str)
